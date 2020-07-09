6798c9930f44e5ff43a5f4e01a5d5d7a54cdc535
David Melnychuk <melnychu@sfu.ca>
2020-07-08 19:28:13 -0700

    add file upload for 2 different files

--- autoML/client/src/App.js
+++ autoML/client/src/App.js
@@ -3,10 +3,12 @@ import FileUpload from './components/FileUpload';
 import axios from "axios";
 
 const App = () => {
-  const [file, setFile] = useState('');
-  const [filename, setFilename] = useState('');
+  const [fileOne, setFileOne] = useState('');
+  const [filenameOne, setFilenameOne] = useState('');
+  const [fileTwo, setFileTwo] = useState('');
+  const [filenameTwo, setFilenameTwo] = useState('');
 
-  const handleFileChange = (event) => {
+  const handleFileChange = (event, setFile, setFilename) => {
     const newFile = event.target.files[0];
     if(newFile !== undefined){
       setFile(newFile);
@@ -14,24 +16,27 @@ const App = () => {
     }
   }
 
-  const handleUpload = (event) => {
+  const handleUpload = (event, file) => {
     event.preventDefault();
     console.log(file);
-    console.log(filename)
 
     const formData = new FormData();
     formData.append('file', file);
     // TODO: Add try/catch, log error, extract URL to constant, add to service.
-    axios.post("http://localhost:8080/upload", formData);
+    axios.post("http://localhost:8080/upload", formData, {
+      onUploadProgress: progressEvent => {
+        console.log(progressEvent.loaded / progressEvent.total)
+      }
+    });
   }
 
   return (
     <div>
       <h1>Upload your files for Class 1</h1>
       {/* <FileUpload uploadFile = {uploadFile}/> */}
-      <FileUpload handleFileChange={handleFileChange} handleUpload={handleUpload}/>
+      <FileUpload handleFileChange={(e) => handleFileChange(e, setFileOne, setFilenameOne)} handleUpload={(e) => handleUpload(e,fileOne)}/>
       <h1> Upload your files for Class 2</h1>
-      <FileUpload handleFileChange={handleFileChange} handleUpload={handleUpload}/>
+      <FileUpload handleFileChange={(e) => handleFileChange(e, setFileTwo, setFilenameTwo)} handleUpload={(e) => handleUpload(e, fileTwo)}/>
     </div>
   );
 }
